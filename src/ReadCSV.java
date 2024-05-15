ffe0f3595b9acbe935ce95330a144b2b65c8b2c0
ZenKuJa <janneskurzke@gmail.com>
2024-05-15 13:53:53 +0200

    read CSV Proof of Concept working, CSV File Update

--- src/ReadCSV.java
+++ src/ReadCSV.java
@@ -1,10 +1,33 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadCSV {
-    public static void main(String[] args) throws Exception {
-        File mySatisfactoryCSV = new File("data\\SatisfactoryDatenJannesKurzke.CSV");
+    public static void main(String[] args) {
+        File satisfactoryCSV = new File("data\\SatisfactoryDatenJannesKurzke.CSV");
+        ArrayList<String> stringRows = new ArrayList<>();
+        try {
+            Scanner csvReadScanner = new Scanner(satisfactoryCSV);
 
-        Scanner csvReader = new Scanner(null)
+            while (csvReadScanner.hasNext()){
+                String row = csvReadScanner.next();
+                stringRows.add(row);
+            }
+
+            csvReadScanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+/*         for (String row: stringRows){
+            for (String column: row.split(";")){
+                System.out.println(column + "------");
+            }
+            System.out.println("******************************");
+        } */
+        for (String columnHead: stringRows.get(0).split(";")){
+            System.out.println(columnHead);
+        }
     }
 }
