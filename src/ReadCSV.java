269468e352f817b68317a4a8fa4fcc256cf0985b
ZenKuJa <janneskurzke@gmail.com>
2024-05-15 13:11:23 +0200

    ordner Struktur fix

---
+++ src/ReadCSV.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ReadCSV {
+    public static void main(String[] args) throws Exception {
+        File mySatisfactoryCSV = new File("data\\SatisfactoryDatenJannesKurzke.CSV");
+
+        Scanner csvReader = new Scanner(null)
+    }
+}
