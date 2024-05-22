add4feb13276b7da00f0e3ec5cc23e7f9beb281c
ZenKuJa <janneskurzke@gmail.com>
2024-05-22 09:08:23 +0200

    removed old Aggregator, keeps HashMap Aggregator

--- src/Aggregator.java
+++ src/Aggregator.java
@@ -1,25 +1,48 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Aggregator {
-    public static ArrayList<Row> aggregate(ArrayList<Row> rows){
-        ArrayList<Row> aggregatedRows = new ArrayList<>();
-        Row currentRecipe = rows.get(0);
-        int uniqueRecipes = 1;
-        for (int i = 0; i < rows.size(); i++) {
-            if (0 == currentRecipe.Recipe.compareTo(rows.get(i).Recipe)){
-                System.out.println(rows.get(i).Recipe + " == " + currentRecipe.Recipe);
-                System.out.println("***********************************");
-            } else if (0 > currentRecipe.Recipe.compareTo(rows.get(i).Recipe)){
-                System.out.println(rows.get(i).Recipe + " != " + currentRecipe.Recipe);
-                currentRecipe = rows.get(i);
-                uniqueRecipes += 1;
-                System.out.println(rows.get(i).Recipe + " == " + currentRecipe.Recipe);
-                System.out.println("***********************************");
+public class AggegatorHashMap {
+    public static ArrayList<Row> aggregate(ArrayList<Row> rows) {
+
+        ArrayList<Row> aggegatedRows = new ArrayList<>();
+
+        HashMap<String, ArrayList<Row>> uniqueRecipes = new HashMap<>();
+
+        for (Row row : rows) {
+            if (!uniqueRecipes.containsKey(row.Recipe)) {
+                uniqueRecipes.put(row.Recipe, new ArrayList<>());
+            }
+            uniqueRecipes.get(row.Recipe).add(row);
+        }
+
+        for (Map.Entry<String, ArrayList<Row>> entry: uniqueRecipes.entrySet()) {
+            System.out.println(entry.getKey());
+            for (Row row : entry.getValue()) {
+                System.out.println(row.Recipe + " " + row.RatePerMinOut1);
+            }
+            System.out.println("****************");
+
+            if (entry.getValue().size() > 1){
+                aggegatedRows.add(getHighestProductionRate(entry.getValue()));
+            } else {
+                aggegatedRows.add(entry.getValue().get(0));
             }
         }
 
-        System.out.println(uniqueRecipes);
+        System.out.println(uniqueRecipes.size());
+
+        return aggegatedRows;
+    }
+
+    private static Row getHighestProductionRate(ArrayList<Row> rows){
+        Row maxRow = new Row();
         
-        return aggregatedRows;
+        for (Row row: rows){
+            if (row.RatePerMinOut1 > maxRow.RatePerMinOut1){
+                maxRow = row;
+            }
+        }
+        return maxRow;
     }
 }
