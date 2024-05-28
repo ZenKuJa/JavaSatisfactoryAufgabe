50a55c1670f673e2606fab961c2b999cf112109a
ZenKuJa <janneskurzke@gmail.com>
2024-05-28 09:08:52 +0200

    nothing fancy

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 public class Sorter {
+    /** Sortiert die gegebene ArrayList"<Row>" absteigend nach der
+    RatePerMinOut1 eines jeden Rezeptes **/
     public static void sort(ArrayList<Row> rows){
         int length = rows.size();
-        if (length < 2){
+        if (length < 2){ //Abbruchbedingung der Rekursion
             return;
         }
+        //Split als Grundlage von Merge Sort
         int middleIndex = length / 2;
         ArrayList<Row> leftSide = new ArrayList<>();
         ArrayList<Row> rightSide = new ArrayList<>();
@@ -16,11 +19,14 @@ public class Sorter {
                 rightSide.add(rows.get(i));
             }
         }
+        //Rekursiver Aufruf für die jeweilige Seite
         sort(leftSide);
         sort(rightSide);
-        merge(rows, leftSide, rightSide);
+        merge(rows, leftSide, rightSide); //Tatsächliches sortieren
     }
 
+    /** Sortiert zwei gegeben ArrayList<Row> in ein Output
+    ArrayList<Row> nach RatePerMinOut1 **/
     private static void merge(ArrayList<Row> rows, ArrayList<Row> leftSide, ArrayList<Row> rightSide) {
         int leftIndex = 0;
         int rightIndex = 0;
@@ -33,11 +39,13 @@ public class Sorter {
                 rows.set(i, leftSide.get(leftIndex));
                 leftIndex += 1;
                 continue;
-            } else if (leftSide.get(leftIndex).RatePerMinOut1 >= rightSide.get(rightIndex).RatePerMinOut1){
+            } else if (leftSide.get(leftIndex).RatePerMinOut1 >= 
+                            rightSide.get(rightIndex).RatePerMinOut1){
                 rows.set(i, leftSide.get(leftIndex));
                 leftIndex += 1;
                 continue;
-            } else if (leftSide.get(leftIndex).RatePerMinOut1 <= rightSide.get(rightIndex).RatePerMinOut1){
+            } else if (leftSide.get(leftIndex).RatePerMinOut1 <= 
+                            rightSide.get(rightIndex).RatePerMinOut1){
                 rows.set(i, rightSide.get(rightIndex));
                 rightIndex += 1;
                 continue;
